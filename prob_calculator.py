894f2c14ea99087ad2e28b2fef54145b77d7f765
Maya Stevenson <maya.stevenson@gmail.com>
2022-08-31 15:58:56 -0600

    Finished experiment function

--- prob_calculator.py
+++ prob_calculator.py
@@ -5,22 +5,31 @@ class Hat:
         self.contents = list()
         for key, val in kwargs.items():
             for i in range(val): self.contents.append(key)
+        self.og_contents = copy.copy(self.contents) # copy original contents for resetting hat 
 
     # removes n balls from hat without replacement; return string containing removed balls
     def draw(self, n):
-        #random.sample(population, k, *, counts=None)
-        random.shuffle(self.contents)
-        removed_balls = self.contents[:n]
-        del self.contents[:n]
+        # commented code below did not work with test_module (but worked in my testing), I believe we need to use random.sample to have an exact match 
+        #random.shuffle(self.contents)
+        #removed_balls = self.contents[:n]
+        #del self.contents[:n]
+        if n>len(self.contents): n = len(self.contents)
+        removed_balls = random.sample(self.contents, n)
+        for ball in removed_balls: self.contents.remove(ball)
         return removed_balls
 
-#def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    def reset(self):
+        self.contents = copy.copy(self.og_contents)
 
-
-# test -----------------------------------------
-hat1 = Hat(yellow=3, blue=2, green=6)
-hat2 = Hat(red=5, orange=4)
-hat3 = Hat(red=5, orange=4, black=1, blue=0, pink=2, striped=9)
-print(hat1.contents)
-#print(hat2.contents)
-print(hat1.draw(5), hat1.contents)
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    correct = 0
+    for i in range(num_experiments):
+        drawn_balls = hat.draw(num_balls_drawn)
+        check = True
+        for key, val in expected_balls.items():
+            if drawn_balls.count(key) < val:
+                check = False
+                break
+        if check: correct += 1
+        hat.reset()        
+    return correct/num_experiments
